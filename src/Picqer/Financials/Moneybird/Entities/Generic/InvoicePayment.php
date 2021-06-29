7d8b68281bfafcdd168c40b1703e504f1d67f439
Fabian van Schevikhoven <fabian@uptimize.nl>
2021-06-29 15:07:22 +0200

    Added ledger_account_id as fillable entry @ Invoice Payment

--- src/Picqer/Financials/Moneybird/Entities/Generic/InvoicePayment.php
+++ src/Picqer/Financials/Moneybird/Entities/Generic/InvoicePayment.php
@@ -7,31 +7,31 @@ use Picqer\Financials\Moneybird\Model;
 /**
  * Class InvoicePayment.
  */
-abstract class InvoicePayment extends Model
-{
-    /**
-     * @var array
-     */
-    protected $fillable = [
-        'id',
-        'invoice_type',
-        'invoice_id',
-        'financial_account_id',
-        'user_id',
-        'payment_transaction_id',
-        'price',
-        'price_base',
-        'payment_date',
-        'credit_invoice_id',
-        'financial_mutation_id',
-        'transaction_identifier',
-        'manual_payment_action',
-        'created_at',
-        'updated_at',
-    ];
+abstract class InvoicePayment extends Model {
+	/**
+	 * @var array
+	 */
+	protected $fillable = [
+		'id',
+		'invoice_type',
+		'invoice_id',
+		'financial_account_id',
+		'user_id',
+		'payment_transaction_id',
+		'price',
+		'price_base',
+		'payment_date',
+		'credit_invoice_id',
+		'financial_mutation_id',
+		'transaction_identifier',
+		'manual_payment_action',
+		'ledger_account_id',
+		'created_at',
+		'updated_at',
+	];
 
-    /**
-     * @var string
-     */
-    protected $namespace = 'payment';
+	/**
+	 * @var string
+	 */
+	protected $namespace = 'payment';
 }
